13a64ff9628f5d69ee2f17402c7ad2f86775ece2
devRonPark <pbc9236@gmail.com>
2023-03-01 15:49:04 +0900

    feat: id로 포켓몬 정보 조회하는 로직 구현

--- src/pokemon/pokemon.module.ts
+++ src/pokemon/pokemon.module.ts
@@ -1,7 +1,9 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { PokemonService } from './pokemon.service';
 
 @Module({
-  providers: [PokemonService]
+  imports: [HttpModule],
+  providers: [PokemonService],
 })
 export class PokemonModule {}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @Injectable()
-export class PokemonService {}
+export class PokemonService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async getPokemon(id: number) {
+    if (id < 1 || id > 151) throw new BadRequestException('Invalid Pokemon ID');
+
+    const { data } = await this.httpService.axiosRef({
+      url: `https://pokeapi.co/api/v2/pokemon/${id}`,
+      method: 'GET',
+    });
+
+    if (!data || !data.species || !data.species.name) {
+      throw new InternalServerErrorException();
+    }
+
+    return data.species.name;
+  }
+}
